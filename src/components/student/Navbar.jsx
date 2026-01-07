2a9099e76d423c5d1b94fe2feffeee0035118598
Currents <currentsaisyah@gmail.com>
2026-01-08 06:41:07 +0700

    fix the navbar position

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -12,14 +12,14 @@ const Navbar = () => {
     : 'bg-cyan-100/70'}`}>
         <img src ={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
         <div className='hidden md:flex items-center gap-5 text-gray-500'>
-            <div>
+            <div className='flex items-center gap-5'>
               <button>Become educator</button>
               |  <Link to='/my-enrollments'> My Enrollments</Link>
             </div>
             <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>
               Create Account</button>
         </div>
-        <div></div>
+        <div className='md:hidden'></div>
     </div>
   )
 }
