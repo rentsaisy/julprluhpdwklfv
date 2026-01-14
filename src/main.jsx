f36f24c8a9fb3ee24ee043b8aaff1e9c7f53229c
Currents <currentsaisyah@gmail.com>
2026-01-15 04:51:21 +0700

    add clerk in project

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { ClerkProvider } from "@clerk/clerk-react";
 import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
 import {BrowserRouter} from 'react-router-dom'
 
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>,
-)
+  <React.StrictMode>
+      <ClerkProvider publishableKey={clerkPubKey} afterSignOutUrl='/'>
+        <BrowserRouter>
+          <AppContextProvider>
+            <App />
+          </AppContextProvider>
+        </BrowserRouter>
+      </ClerkProvider>
+  </React.StrictMode>
+);
