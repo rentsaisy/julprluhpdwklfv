c875f60076ce0bd95601cac5822877083c38415c
Currents <currentsaisyah@gmail.com>
2026-01-15 08:51:01 +0700

    change color theme into purple

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -13,15 +13,15 @@ const Navbar = () => {
 
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 
-    lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' 
-    : 'bg-cyan-100/70'}`}>
+    lg:px-36 border-b border-purple-200 py-4 ${isCourseListPage ? 'bg-white' 
+    : 'bg-purple-50'}`}>
         <img src ={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
         <div className='hidden md:flex items-center gap-5 text-gray-500'>
             <div className='flex items-center gap-5'>
               <button>Become educator</button>
-              |  <Link to='/my-enrollments'> My Enrollments</Link>
+              |  <Link to='/my-enrollments' className='text-purple-900 hover:text-purple-700 transition-colors'> My Enrollments</Link>
             </div>
-            <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>
+            <button onClick={()=> openSignIn()} className='bg-purple-600 text-white px-5 py-2 rounded-full'>
               Create Account</button>
         </div>
         {/* For phone screens */}
